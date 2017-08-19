b91ff7f08e490eaafb57530c33c2e3243035613e
gaolinhua <gaolinhua@bjfengteng.com>
2017-08-19 13:46:04 +0800

    modify develop

--- app/src/main/java/com/example/gaolinhua/gtest/MainActivity.java
+++ app/src/main/java/com/example/gaolinhua/gtest/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e(TAG, "onCreate: ");
-        Log.e(TAG, "onCreate:==== ");
+        Log.e(TAG, "onCreate: develop");
     }
 }
---
+++ app/src/main/java/com/example/gaolinhua/gtest/Test.java
@@ -0,0 +1,10 @@
+package com.example.gaolinhua.gtest;
+
+/**
+ * Created by gaolinhua on 17/8/19.
+ */
+
+public class Test {
+    private int age;
+    private String name;
+}
